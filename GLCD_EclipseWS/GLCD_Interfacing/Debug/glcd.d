ba9281b30ee564bb2ff290ddf6e35d674f97c8d5
Halieeim <ahmedlpl86@gmail.com>
2022-12-31 13:59:13 +0200

    initial commit

---
+++ GLCD_EclipseWS/GLCD_Interfacing/Debug/glcd.d
@@ -0,0 +1,10 @@
+glcd.o glcd.o: ../glcd.c ../glcd.h ../gpio.h ../std_types.h \
+  ../common_macros.h
+
+../glcd.h:
+
+../gpio.h:
+
+../std_types.h:
+
+../common_macros.h:
---
+++ GLCD_EclipseWS/GLCD_Interfacing/Debug/main.d
@@ -0,0 +1,10 @@
+main.o main.o: ../main.c ../glcd.h ../gpio.h ../std_types.h \
+  ../common_macros.h
+
+../glcd.h:
+
+../gpio.h:
+
+../std_types.h:
+
+../common_macros.h:
